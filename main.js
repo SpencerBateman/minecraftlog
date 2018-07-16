6fc339bea77db5401ad90e1eb1f1ef2a54a152a1
Spencer Bateman <bateman.s@husky.neu.edu>
2018-07-15 23:12:24 -0700

    some cosmetic changes

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ $.get('latest.log', { "_": $.now() }, function (data) {
   arrayOfLines = data.match(/[^\r\n]+/g);
   arrayOfLines.reverse();
   for (var line of arrayOfLines) {
-    console.log(line)
+    //console.log(line)
     time = line.split(' ', 1)
     time  = time[0].slice(1, -1)
-    sender = line.split(/]: (.+)/)[1]
-    sender = line.split(']:', 1)
-    sender = sender[0].split('] [', 2)
-    sender = sender[1]
-    console.log(sender)
-    text = line.split(/]: (.+)/)[1]
-    $('.log__list').append('<li class="log__item"><span class="time">' + time + '</span><span class="' + sender.substring(sender.length - 4)+'">' + sender + '</span><span class="text">' + text + '</span></li>')
+    text = line.split(']: ', 2)[1]
+    text = text.replace('<', '')
+    text = text.replace('>', '')
+    console.log(time)
+    myDate = new Date(time)
+    console.log(myDate)
+    $('.log__list').append('<li class="log__item"><span class="time">' + time + '</span>' + '</span><span class="text">  -- ' + text + '</span></li>')
   }
 });
 
